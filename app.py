488f413ccd6e57a32543b7e4e360e8b61ef51c69
berkaygurcan <berkaygurcan141@gmail.com>
2020-03-08 17:10:41 +0300

    Fixed not importing db when running the app.

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =  False #flaskın özelligin kapat
 app.secret_key = "jose"
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+#@app.before_first_request run.py oluşturuldu heroku içi burası hata alıyordu
+#def create_tables():
+ #   db.create_all()
 
 jwt = JWT(app,authenticate,identity)#/auth
 
